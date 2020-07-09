2fd7deace7a9b43653f87835fcd7be48b22b4c0c
gbresolin <gregory.bresolin@ipilyon.net>
2020-07-09 17:13:30 +0200

    Mise à jour BDD

--- book/schema.sql
+++ book/schema.sql
@@ -5,7 +5,8 @@ DROP TABLE IF EXISTS category;
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   username TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL
+  password TEXT NOT NULL,
+  isAdmin INTEGER NOT NULL DEFAULT 0
 );
 
 CREATE TABLE product (
@@ -28,3 +29,12 @@ CREATE TABLE category (
   author_id INTEGER NOT NULL,
   FOREIGN KEY (author_id) REFERENCES user (id)
 );
+
+INSERT INTO user(id,username,password,isAdmin) VALUES('1', 'gregory', 'pbkdf2:sha256:150000$PU2JIbfY$c5f0273db55cd9e1911852298d2a985c7c6bac9ebcf0c368852061abb4a5a6e7',1);
+INSERT INTO user(id,username,password,isAdmin) VALUES('2', 'george', 'pbkdf2:sha256:150000$PU2JIbfY$c5f0273db55cd9e1911852298d2a985c7c6bac9ebcf0c368852061abb4a5a6e7',1);
+INSERT INTO user(id,username,password,isAdmin) VALUES('3', 'tony', 'pbkdf2:sha256:150000$VgMV7kx2$9b6414370ed48f11355f4e90781594ed8ce9406a040245142299d32f1bd57ed5',0);
+INSERT INTO product(id,author_id,category_id,name,description,price,state,image) VALUES('1','1','1','Le pays des autres','Un livre bien écrit à découvrir','25','Neuf','book_220320201306.jpg');
+INSERT INTO product(id,author_id,category_id,name,description,price,state,image) VALUES('2','1','3','Dragon Ball Super - Tome 11','Le combat contre le terrible criminel Moro fait toujours rage, et le Dai Kaio Shin endormi au fond de Boo s’est même joint à la danse. Le lieu de l’action quitte le vide sidéral pour se déplacer sur la nouvelle planète Namek.','6.9','Neuf','book_090720204834.jpg');
+INSERT INTO category(id,name,author_id) VALUES('1', 'Histoire', '1');
+INSERT INTO category(id,name,author_id) VALUES('2', 'Humour', '1');
+INSERT INTO category(id,name,author_id) VALUES('3', 'Manga', '1');
